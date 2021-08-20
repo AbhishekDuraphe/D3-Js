c221d1a6a31591104231791808172ea8025e47d9
AbhishekDuraphe <abhishek.duraphe@gmail.com>
2021-08-20 23:08:53 +0530

    Added stacked bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { Section28Component } from './section-2-8/section-2-8.component';
 import { StarbucksCoffeeProjectComponent } from './starbucks-coffee-project/starbucks-coffee-project.component';
+import { StackedBarChartComponent } from './stacked-bar-chart/stacked-bar-chart.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, Section28Component, StarbucksCoffeeProjectComponent ],
+  declarations: [ AppComponent, HelloComponent, Section28Component, StarbucksCoffeeProjectComponent , StackedBarChartComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
